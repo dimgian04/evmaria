e74501b6fbef8a6c7d74820aab6eb3612ef23107
dimgian04 <117597916+dimgian04@users.noreply.github.com>
2025-09-26 18:55:39 +0300

    Add new dependencies and update server port
    Added body-parser, dotenv, and nodemailer to dependencies in package.json. Changed the default server port in server.js from 3001 to 3000.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
 
